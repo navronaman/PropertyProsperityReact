f2915a2f5e9be957dedca515759fe876de9790c9
Dhruv Daiya <111184168+Dhruvedd@users.noreply.github.com>
2024-02-11 00:35:34 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import LandingPage from "./LandingPage"
+import index from "./index.css"
+
+export default function App() {
+  return (
+    <div><LandingPage /></div>
+  )
+}
